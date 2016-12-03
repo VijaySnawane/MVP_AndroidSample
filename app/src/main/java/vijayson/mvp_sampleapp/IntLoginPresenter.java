55591da42ea5329177ce9e4a68a492b31cb08136
VijaySnawane <sonawane.vijay05@gmail.com>
2016-12-03 07:48:32 +0530

    Update IntLoginPresenter.java

--- app/src/main/java/vijayson/mvp_sampleapp/IntLoginPresenter.java
+++ app/src/main/java/vijayson/mvp_sampleapp/IntLoginPresenter.java
@@ -4,5 +4,9 @@ package vijayson.mvp_sampleapp;
  * Created by monika-Asus-pc on 4/3/2016.
  */
 public interface IntLoginPresenter {
+    
+    
+    
+    
     public void doLogin(String username,String Password);
 }
